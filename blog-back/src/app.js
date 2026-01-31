b0f5b2e814bf8f40f99dc7ce63bd3fe70431d438
Baloram Roy <baloramroyram@gmail.com>
2026-02-01 00:07:27 +0600

    add docker file and ignore file and some others

--- blog-back/src/app.js
+++ blog-back/src/app.js
@@ -41,4 +41,9 @@ mongoose.connect(process.env.MONGO_URI)
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
